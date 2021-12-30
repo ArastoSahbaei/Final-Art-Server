711fb2d7d423c06f7dd452e9be15d5141abd93f7
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-12-30 14:17:47 +0100

    m

--- Server.ts
+++ Server.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from './src/configurations/functions/connectToDatab
 import { connectToPort } from './src/configurations/functions/connectToPort'
 import webSocketServer from 'websocket'
 import http from 'http'
+import { getUniqueID } from './src/configurations/functions/getUniqueID'
 /* import cors from 'cors' */
 
 const application = express()
@@ -23,26 +24,23 @@ const wsServer = new webSocketServer.server({
 
 const clients = {}
 
-const getUniqueID = () => {
-	const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
-	return s4() + s4() + '-' + s4()
-}
-
 wsServer.on('request', function (request) {
-	const userId = getUniqueID()
-	console.log((new Date()) +  'Recieved a new connection from origin ' + request.origin + '.')
+	const userID = getUniqueID()
+	console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.'+ `ID: ${userID}`)
 
+
+	// You can rewrite this part of the code to accept only the requests from allowed origin
 	const connection = request.accept(null, request.origin)
-	clients[userId] = connection
-	console.log('connected' + userId + 'in' + Object.getOwnPropertyNames(clients))
+	clients[userID] = connection
+	/* console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients)) */
 
-	connection.on('message', function(message) {
-		if(message.type === 'utf8') {
-			console.log('Received message: ', message.utf8Data)
+	connection.on('message', function (message) {
+		if (message.type === 'utf8') {
+		/* 	console.log('Received Message: ', message.utf8Data) */
 
-			for(const key in clients) {
+			// broadcasting message to all connected clients
+			for (const key in clients) {
 				clients[key].sendUTF(message.utf8Data)
-				console.log('sent message to: ', clients[key])
 			}
 		}
 	})
